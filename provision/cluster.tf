8d30a3f690c3125fe95e143344e8d9f33fd93e55
Erik Silva <erikyami@gmail.com>
2023-05-14 10:31:33 -0300

    Ajustes gitignore - argo apps

--- provision/cluster.tf
+++ provision/cluster.tf
@@ -67,3 +67,39 @@ resource "kubernetes_secret" "create_git_private_repo_secret" {
     kind_cluster.default
   ]
 }
+
+## admin@123
+resource "null_resource" "update_argocd_password" {
+  triggers = {
+    key = uuid()
+  }
+  
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\Updating argocd password...\n"
+      kubectl -n ${helm_release.argocd.namespace} patch secret argocd-secret \
+        -p '{"stringData": {
+          "admin.password": "$2a$12$WkH.eHw1XdRD7G6WDgwuBeAzneW4VQqjFsEmgH0BcS.hKLaJ1gSF6",
+          "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+        }}'
+    EOF
+  }
+
+depends_on = [ 
+  kubernetes_secret.create_git_private_repo_secret
+ ]
+}
+
+resource "null_resource" "create_root_argoapp" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\nCreating root argoapp...\n"
+      kubectl --namespace ${helm_release.argocd.namespace} \
+        apply -f ./files/argocd-root-app.yaml
+    EOF
+  }
+}
